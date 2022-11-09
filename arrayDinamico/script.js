0785a025359bce5d2de23659317b8ced6ef8fdc2
Cleyton Silva <silva.rcleyton@gmail.com>
2022-11-09 16:01:22 -0300

    fase 2 finalizada

--- arrayDinamico/script.js
+++ arrayDinamico/script.js
@@ -1,6 +1,16 @@
-array = [0, 0, 0]
-for (let i = 0; i < array.length; i++ ) {
-  array[i] = parseInt(Math.random() * 4)
-}
+function dinamic() {
 
-console.log(`Valores dinâmicos ${array}`)
+  array = [0, 0, 0]
+
+  for (let i = 0; i < array.length; i++ ) {
+    array[i] = parseInt(Math.random() * 3 + 1)
+  }
+
+  if (array[0] == array[1] && array[0] == array[2]) {
+    console.log("Porta 1: aberta")
+  } else {
+    console.log("Tente de novo")
+  }
+
+  return array
+}
